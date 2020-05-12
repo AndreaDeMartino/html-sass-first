f7d1955493afe2d77fa8f503a05b7ac9846b1dda
Andrea De Martino <ademartino1992@gmail.com>
2020-05-12 15:42:54 +0200

    Create: Basic Task

---
+++ dist/script/main.js
@@ -0,0 +1,24 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	/************************************************************************/
+(() => {
+/*!****************************!*\
+  !*** ./src/script/main.js ***!
+  \****************************/
+/*! unknown exports (runtime-defined) */
+/*! exports [maybe provided (runtime-defined)] [unused] */
+/*! runtime requirements:  */
+console.log('ciao');
+})();
+
+(() => {
+/*!****************************!*\
+  !*** ./src/scss/main.scss ***!
+  \****************************/
+/*! unknown exports (runtime-defined) */
+/*! exports [maybe provided (runtime-defined)] [unused] */
+/*! runtime requirements:  */
+// extracted by mini-css-extract-plugin
+})();
+
+/******/ })()
+;
